957f8a4aade9e6d8137a82e0d259dbf758c868ec
Jkue1 <35712462+Jkue1@users.noreply.github.com>
2018-01-31 18:19:34 -0800

    Update madlibs.cpp

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,30 @@
-//Authors: 
+//Authors:Joshua Kue 
 #include <iostream>
 #include <string>
 
 int main()
 {
+    std::string noun;
+    std::cout <<"Enter a noun"<<std::endl;
+    std::cin >>noun;
+
+int nouns;
+   std::string nouns
+   std::cout <<"Enter a different nouns"<<std::endl;
+   std::cin >>nouns;
+
+char letter;
+   std::cout <<"Enter a letter"<<std::endl;
+   std::cin >>letter;
+
+int number;
+   std::cout <<"Enter a number"<<std::endl;
+   std::cin >>number;
+
+  std::cout << "I have a" <<noun<< ", I have a" <<nouns<<"" << std::endl;
+  std::cout << "Uh! "<<noun<<""<<nouns<<"" << std::endl;
+  std::cout << "I have a "<<letter<<", I have a "<<number<<"" << std::endl;
+  std::cout << "Uh! "<<letter<<""<<number<<"."<< std::endl;
 
   return 0;
 }
